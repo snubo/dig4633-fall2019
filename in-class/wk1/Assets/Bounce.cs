1cdf7ee9fdd2bb2989500af56d60a14a4cfd89fc
snubo <lais.marques@outlook.com>
2019-09-18 17:23:48 -0400

    Relearned how to code kinda
    Depression

---
+++ in-class/wk1/Assets/Bounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    float Cube =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cube = Cube + Time.deltaTime;
+        gameObject.transform.Translate(0, Time.deltaTime, 0);
+    }
+}
